b163b143be37fb9cf479d7b61d04a6c5afcfac84
Martin Bosch <92443557+MbochaL@users.noreply.github.com>
2022-06-28 02:55:31 -0300

    Add files via upload

---
+++ Ejercicio-02/Ejercicio-Lista.js
@@ -0,0 +1,14 @@
+//Ejercicio
+const FechaDeNacimiento = new Date(2001, 3, 27) 
+console.log(FechaDeNacimiento)
+
+const LibroFav = {
+    titulo: 'El señor de los anillos',
+    autor: 'J.R.R. Tolkien',
+    Fecha: 1954,
+    URL: "https://www.amazon.com/-/es/J-R-Tolkien/dp/8445000667",
+}
+
+const ejercicio = ["Martin", 21, true, FechaDeNacimiento, LibroFav]
+console.log(ejercicio)
+
