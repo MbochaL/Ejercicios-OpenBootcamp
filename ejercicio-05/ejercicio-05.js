045a45f950afb76b7138490c99d3b7b092c3c3e4
Martin Bosch <92443557+MbochaL@users.noreply.github.com>
2022-07-01 04:26:47 -0300

    Add files via upload

---
+++ ejercicio-05/ejercicio-05.js
@@ -0,0 +1,21 @@
+// Altura en centimetros
+let altura_Centimetros = 100;
+
+// Altura en metros
+let altura_Metros = 25.7;
+
+// Peso en Kilogramos
+let peso_Kilogramos = 94.33;
+
+// Redondeo altura_Metros hacia arriba
+let altura_Metros_Redondeada = altura_Metros;
+console.log(Math.round(altura_Metros_Redondeada));
+
+// Redondeo peso_Kilogramos hacia abajo
+let peso_Kilogramos_Redondeado = peso_Kilogramos;
+console.log(Math.floor(peso_Kilogramos_Redondeado));
+
+// Una variable con el maximo valor que se puede obtener en javascript
+let max_Valor = Number.MAX_VALUE;
+
+console.log(max_Valor + 1);
